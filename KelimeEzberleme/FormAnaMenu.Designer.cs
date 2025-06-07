5baec09d75f53b5b4a632f154963693a10bf0ffd
beyzaincehasan <beyzaincehasan@gmail.com>
2025-06-07 17:14:05 +0300

    ana menü formu ön yüz tasarımı yapıldı.

--- KelimeEzberleme/FormAnaMenu.Designer.cs
+++ KelimeEzberleme/FormAnaMenu.Designer.cs
@@ -29,98 +29,124 @@ namespace KelimeEzberleme
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.labelLogo = new System.Windows.Forms.Label();
             this.button_Ayarlar = new System.Windows.Forms.Button();
-            this.button_Profil = new System.Windows.Forms.Button();
             this.button_Rapor = new System.Windows.Forms.Button();
             this.button_KelimeEkle = new System.Windows.Forms.Button();
             this.button_KelimeEzberle = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
+            this.panelMenu.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
+            // panelMenu
             // 
-            this.panel1.Controls.Add(this.button_Ayarlar);
-            this.panel1.Controls.Add(this.button_Profil);
-            this.panel1.Controls.Add(this.button_Rapor);
-            this.panel1.Controls.Add(this.button_KelimeEkle);
-            this.panel1.Controls.Add(this.button_KelimeEzberle);
-            this.panel1.Location = new System.Drawing.Point(12, 5);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(149, 962);
-            this.panel1.TabIndex = 0;
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(245)))), ((int)(((byte)(245)))));
+            this.panelMenu.Controls.Add(this.labelLogo);
+            this.panelMenu.Controls.Add(this.button_Ayarlar);
+            this.panelMenu.Controls.Add(this.button_Rapor);
+            this.panelMenu.Controls.Add(this.button_KelimeEkle);
+            this.panelMenu.Controls.Add(this.button_KelimeEzberle);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(233, 764);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // labelLogo
+            // 
+            this.labelLogo.AutoSize = true;
+            this.labelLogo.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold);
+            this.labelLogo.ForeColor = System.Drawing.Color.SeaGreen;
+            this.labelLogo.Location = new System.Drawing.Point(44, 27);
+            this.labelLogo.Name = "labelLogo";
+            this.labelLogo.Size = new System.Drawing.Size(113, 46);
+            this.labelLogo.TabIndex = 99;
+            this.labelLogo.Text = "Menü";
             // 
             // button_Ayarlar
             // 
-            this.button_Ayarlar.BackgroundImage = global::KelimeEzberleme.Properties.Resources.automation_80px;
-            this.button_Ayarlar.Location = new System.Drawing.Point(26, 787);
+            this.button_Ayarlar.BackColor = System.Drawing.Color.SeaGreen;
+            this.button_Ayarlar.FlatAppearance.BorderSize = 0;
+            this.button_Ayarlar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_Ayarlar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.button_Ayarlar.ForeColor = System.Drawing.Color.White;
+            this.button_Ayarlar.Location = new System.Drawing.Point(25, 324);
             this.button_Ayarlar.Name = "button_Ayarlar";
-            this.button_Ayarlar.Size = new System.Drawing.Size(111, 78);
-            this.button_Ayarlar.TabIndex = 4;
+            this.button_Ayarlar.Size = new System.Drawing.Size(160, 48);
+            this.button_Ayarlar.TabIndex = 3;
             this.button_Ayarlar.Text = "Ayarlar";
-            this.button_Ayarlar.UseVisualStyleBackColor = true;
+            this.button_Ayarlar.UseVisualStyleBackColor = false;
             this.button_Ayarlar.Click += new System.EventHandler(this.button_Ayarlar_Click);
-            // 
-            // button_Profil
-            // 
-            this.button_Profil.Location = new System.Drawing.Point(26, 871);
-            this.button_Profil.Name = "button_Profil";
-            this.button_Profil.Size = new System.Drawing.Size(111, 78);
-            this.button_Profil.TabIndex = 3;
-            this.button_Profil.Text = "Profil";
-            this.button_Profil.UseVisualStyleBackColor = true;
             // 
             // button_Rapor
             // 
-            this.button_Rapor.Location = new System.Drawing.Point(14, 193);
+            this.button_Rapor.BackColor = System.Drawing.Color.SeaGreen;
+            this.button_Rapor.FlatAppearance.BorderSize = 0;
+            this.button_Rapor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_Rapor.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.button_Rapor.ForeColor = System.Drawing.Color.White;
+            this.button_Rapor.Location = new System.Drawing.Point(25, 247);
             this.button_Rapor.Name = "button_Rapor";
-            this.button_Rapor.Size = new System.Drawing.Size(111, 78);
+            this.button_Rapor.Size = new System.Drawing.Size(160, 48);
             this.button_Rapor.TabIndex = 2;
             this.button_Rapor.Text = "Rapor";
-            this.button_Rapor.UseVisualStyleBackColor = true;
+            this.button_Rapor.UseVisualStyleBackColor = false;
             this.button_Rapor.Click += new System.EventHandler(this.button_Rapor_Click);
             // 
             // button_KelimeEkle
             // 
-            this.button_KelimeEkle.Location = new System.Drawing.Point(14, 100);
+            this.button_KelimeEkle.BackColor = System.Drawing.Color.SeaGreen;
+            this.button_KelimeEkle.FlatAppearance.BorderSize = 0;
+            this.button_KelimeEkle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_KelimeEkle.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.button_KelimeEkle.ForeColor = System.Drawing.Color.White;
+            this.button_KelimeEkle.Location = new System.Drawing.Point(25, 174);
             this.button_KelimeEkle.Name = "button_KelimeEkle";
-            this.button_KelimeEkle.Size = new System.Drawing.Size(111, 78);
+            this.button_KelimeEkle.Size = new System.Drawing.Size(160, 48);
             this.button_KelimeEkle.TabIndex = 1;
             this.button_KelimeEkle.Text = "Kelime Ekle";
-            this.button_KelimeEkle.UseVisualStyleBackColor = true;
+            this.button_KelimeEkle.UseVisualStyleBackColor = false;
             this.button_KelimeEkle.Click += new System.EventHandler(this.button_KelimeEkle_Click);
             // 
             // button_KelimeEzberle
             // 
-            this.button_KelimeEzberle.Location = new System.Drawing.Point(14, 16);
+            this.button_KelimeEzberle.BackColor = System.Drawing.Color.SeaGreen;
+            this.button_KelimeEzberle.FlatAppearance.BorderSize = 0;
+            this.button_KelimeEzberle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_KelimeEzberle.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.button_KelimeEzberle.ForeColor = System.Drawing.Color.White;
+            this.button_KelimeEzberle.Location = new System.Drawing.Point(25, 93);
             this.button_KelimeEzberle.Name = "button_KelimeEzberle";
-            this.button_KelimeEzberle.Size = new System.Drawing.Size(111, 78);
+            this.button_KelimeEzberle.Size = new System.Drawing.Size(160, 48);
             this.button_KelimeEzberle.TabIndex = 0;
             this.button_KelimeEzberle.Text = "Kelime Ezberle";
-            this.button_KelimeEzberle.UseVisualStyleBackColor = true;
+            this.button_KelimeEzberle.UseVisualStyleBackColor = false;
             this.button_KelimeEzberle.Click += new System.EventHandler(this.button_KelimeEzberle_Click);
             // 
             // FormAnaMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1503, 979);
-            this.Controls.Add(this.panel1);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1300, 764);
+            this.Controls.Add(this.panelMenu);
             this.Name = "FormAnaMenu";
-            this.Text = "FormAnaMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kelime Ezberleme - Ana Menü";
             this.Load += new System.EventHandler(this.FormAnaMenu_Load);
-            this.panel1.ResumeLayout(false);
+            this.panelMenu.ResumeLayout(false);
+            this.panelMenu.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button_KelimeEkle;
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Label labelLogo;
         private System.Windows.Forms.Button button_KelimeEzberle;
+        private System.Windows.Forms.Button button_KelimeEkle;
         private System.Windows.Forms.Button button_Rapor;
         private System.Windows.Forms.Button button_Ayarlar;
-        private System.Windows.Forms.Button button_Profil;
     }
 }
